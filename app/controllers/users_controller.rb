fdfb33b7b9dce0c3b0709305496e26f9f1cdd26d
OPA <opa@PC094.(none)>
2014-05-16 10:01:51 +0700

    exit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to @user, notice: 'User was 222 successfully created.' }
+       # format.json { render :show222222, status: :created, location: @user }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
--- app/views/users/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @user, :id, :Nam, :email, :created_at, :updated_at
